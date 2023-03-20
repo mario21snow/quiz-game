228fdf3630134baf77555105a8d22246884f829b
MARIYA BILAL SHAIKH <102954942+mario21snow@users.noreply.github.com>
2023-03-20 12:24:36 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "What is the purpose of a favicon in web development?",
+    answer: "To provide a visual representation of a website in a browser tab or bookmark",
+    options: [
+     "To store website content offline",
+     "To provide a secure connection between the server and client",
+     "To provide a visual representation of a website in a browser tab or bookmark",
+     "To track user behavior on a website"
+    ]
+    },
+  
+  {
+      numb: 8,
+      question: "What is the difference between margin and padding in CSS?",
+      answer: "Margin is the space outside of an element's border, while padding is the space inside of an element's border",
+      options: [
+       "Margin is the space inside of an element's border, while padding is the space outside of an element's border",
+       "Margin and padding are the same thing",
+       "Margin is used for horizontal spacing, while padding is used for vertical spacing",
+       "Margin is the space outside of an element's border, while padding is the space inside of an element's border"
+      ]
+      },
+      
+  {
+      numb: 9,
+      question: "What is the difference between a div and a span in HTML?",
+      answer: "A div is a block-level element, while a span is an inline element",
+      options: [
+      "A div is an inline element, while a span is a block-level element",
+      "A div is used for grouping elements together, while a span is used for styling text",
+      "A div is a block-level element, while a span is an inline element",
+      "A div is used for text formatting, while a span is used for creating columns"
+      ]
+      },
+
+      {
+      numb: 10,
+      question: "What is the purpose of the alt attribute in an HTML image tag?",
+      answer: "To provide a text description of the image for accessibility purposes",
+      options: [
+      "To provide a caption for the image",
+      "To provide a link to a larger version of the image",
+      "To provide a text description of the image for accessibility purposes",
+      "To provide a border around the image"
+      ]
+      },
+
+      {
+      numb: 11,
+      question: "What is a variable in programming?",
+      answer: "A named container for storing data that can be manipulated or referenced",
+      options: [
+      "A way to add comments to code for documentation purposes",
+      "A type of function that performs a specific task",
+      "A named container for storing data that can be manipulated or referenced",
+      "A way to create a loop in code"
+      ]
+      }  
+];
